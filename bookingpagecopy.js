08562ea3a84017a76bc9b6aa3bdc5c15b856ee8b
Tanya Ben Philip <tbphilip30@gmail.com>
2022-08-24 14:56:14 +0530

    third commit

--- bookingpagecopy.js
+++ bookingpagecopy.js
@@ -18,86 +18,87 @@
       }, false)
     })
 })()
-  
-      
-function store(){
-  var fname=document.getElementById("firstName").value;
-  localStorage.setItem("firstName",fname);
 
-  var lname=document.getElementById("lastName").value;
-  localStorage.setItem("lastName",lname);
 
-  var age=document.getElementById("age").value;
-  localStorage.setItem("age",age);
+function store() {
+  var fname = document.getElementById("firstName").value;
+  localStorage.setItem("firstName", fname);
 
-  var email=document.getElementById("emailId").value;
-  localStorage.setItem("email",email);
+  var lname = document.getElementById("lastName").value;
+  localStorage.setItem("lastName", lname);
 
-  var pnumber=document.getElementById("phoneNumber").value;
-  localStorage.setItem("phoneNumber",pnumber);
+  var age = document.getElementById("age").value;
+  localStorage.setItem("age", age);
 
-  
-    var gender_value;
-    var gender;
+  var email = document.getElementById("emailId").value;
+  localStorage.setItem("email", email);
 
-    if (document.getElementById("male").checked) {
-    gender_value = document.getElementById("male").value;                 
-    
-    }
-        
-    else if(document.getElementById("female").checked){
-        gender_value = document.getElementById("female").value;                    
-    }
+  var pnumber = document.getElementById("phoneNumber").value;
+  localStorage.setItem("phoneNumber", pnumber);
 
-    else if(document.getElementById("other").checked){
-        gender_value = document.getElementById("other").value;
-    }  
-    gender=localStorage.setItem("gender",gender_value);
-    
+
+  var gender_value;
+  var gender;
+
+  if (document.getElementById("male").checked) {
+    gender_value = document.getElementById("male").value;
+
+  }
+
+  else if (document.getElementById("female").checked) {
+    gender_value = document.getElementById("female").value;
+  }
+
+  else if (document.getElementById("other").checked) {
+    gender_value = document.getElementById("other").value;
+  }
+  gender = localStorage.setItem("gender", gender_value);
+
 
 }
 //to store values of new passenger
-function storeNew(){
-  var fname1=document.getElementById("firstName1").value;
-  localStorage.setItem("firstName1",fname1);
+function storeNew() {
+  var fname1 = document.getElementById("firstName1").value;
+  sessionStorage.setItem("firstName1", fname1);
 
-  var lname1=document.getElementById("lastName1").value;
-  localStorage.setItem("lastName1",lname1);
+  var lname1 = document.getElementById("lastName1").value;
+  sessionStorage.setItem("lastName1", lname1);
 
-  var age1=document.getElementById("age1").value;
-  localStorage.setItem("age1",age1);
+  var age1 = document.getElementById("age1").value;
+  sessionStorage.setItem("age1", age1);
 
 
 
-    var gender_value1;
-    var gender1;
+  var gender_value1;
+  var gender1;
 
-    if (document.getElementById("male").checked) {
-    gender_value1 = document.getElementById("male").value;                 
-    
-    }
-        
-    else if(document.getElementById("female").checked){
-        gender_value1 = document.getElementById("female").value;                    
-    }
+  if (document.getElementById("male1").checked) {
+    gender_value1 = document.getElementById("male").value;
 
-    else if(document.getElementById("other").checked){
-        gender_value1 = document.getElementById("other").value;
-    }  
-    gender1=localStorage.setItem("gender1",gender_value1);
-    
+  }
+
+  else if (document.getElementById("female1").checked) {
+    gender_value1 = document.getElementById("female").value;
+  }
+
+  else if (document.getElementById("other1").checked) {
+    gender_value1 = document.getElementById("other").value;
+  }
+  gender1 = sessionStorage.setItem("gender1", gender_value1);
+
 
 }
 
-function showAddPassenger(){
-  if(document.getElementById('addPassenger').checked){
+function showAddPassenger() {
+  if (document.getElementById('addPassenger').checked) {
     document.getElementById('form1').style = "display:true";
-    document.getElementById('button1').style.display='none';;
+    document.getElementById('button1').style.display = 'none';
+
 
 
   }
-  else{
-    document.getElementById('form1').style.display='none';
+  else {
+    document.getElementById('form1').style.display = 'none';
     document.getElementById('button1').style = "display:true";
 
   }
--- confirmationpage.js
+++ confirmationpage.js
@@ -5,6 +5,10 @@ let det3=localStorage.getItem("age");
 let det4=localStorage.getItem("gender");
 let det5=localStorage.getItem("email");
 let det6=localStorage.getItem("phoneNumber");
+let det7=sessionStorage.getItem("firstName1");
+let det8=sessionStorage.getItem("lastName1");
+let det9=sessionStorage.getItem("age1");
+let det10=sessionStorage.getItem("gender1");
 
 flight_det-=1;
 console.log(flight_det);
@@ -19,7 +23,7 @@ function display_value(data)
 
    content +=  `
    <div class="final_info">
-    <div class="passenger_details" >
+    <div class="details" >
     <h2> Passenger details </h2>
     <h4> Name: ${det1} ${det2}</h4>
     <h4> Age: ${det3} </h4>
@@ -27,7 +31,15 @@ function display_value(data)
     <h4> Email: ${det5}</h4>
     <h4> Phone Number: ${det6}</h4>
     </div>
-    <div class="flight_details" >
+
+    <div style="display:none;" class="details" id="newPassenger" >
+    <h2> Additional Passenger details </h2>
+    <h4> Name: ${det7} ${det8}</h4>
+    <h4> Age: ${det9} </h4>
+    <h4> Gender: ${det10}</h4>
+    </div>
+
+    <div class="details" >
     <h2> Flight details </h2>
     <h4> Flight Number: ${data[flight_det].flightNum}</h4>
     <h4>Origin: <span> ${data[flight_det].origin}</span></h4>
@@ -40,6 +52,14 @@ function display_value(data)
 
 `
 document.querySelector("#card-collection").innerHTML = content;
+
+if (det7!=null){
+    document.getElementById('newPassenger').style = "display:true";
+}
+else{
+    document.getElementById('newPassenger').style.display = none;
+
+}
 }
 
 
